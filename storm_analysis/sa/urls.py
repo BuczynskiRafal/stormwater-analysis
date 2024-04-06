07377258fe5006c0b43b338ab2809d2c6681e12c
BuczynskiRafal <buczynski.rafal2@gmail.com>
2024-04-06 05:39:46 +0200

    Implement Analysis Tab with SWMM File Upload and Frost Zone Form

--- storm_analysis/sa/urls.py
+++ storm_analysis/sa/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import analysis, history
+from .views import analysis, history, upload_file
 
 app_name = "sa"
 
 urlpatterns = [
+    path("analysis/upload/", upload_file, name="upload_file"),
     path("analysis/", analysis, name="analysis"),
     path("history/", history, name="history"),
 ]
