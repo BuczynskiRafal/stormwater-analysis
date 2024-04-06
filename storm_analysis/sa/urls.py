bf8291fc173792ff49688954d003600b22c1eb0c
BuczynskiRafal <buczynski.rafal2@gmail.com>
2024-04-06 05:58:36 +0200

    WPI

--- storm_analysis/sa/urls.py
+++ storm_analysis/sa/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 
-from .views import analysis, history, upload_file
+from .views import analysis, history
 
 app_name = "sa"
 
 urlpatterns = [
-    path("analysis/upload/", upload_file, name="upload_file"),
     path("analysis/", analysis, name="analysis"),
     path("history/", history, name="history"),
 ]
