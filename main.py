c194c7bb40b7c5028338d82f49b64cfacbcbabd7
BuczynskiRafal <buczynski.rafal2@gmail.com>
2024-06-12 06:43:00 +0200

    As in review comments

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import numpy as np
 import pandas as pd
 import pyswmm as ps
 import swmmio as sw
-
-from stormwater_analysis.data.feature_engineering import feature_engineering
-from stormwater_analysis.inp_manage.inp import SwmmModel
 from stormwater_analysis.inp_manage.test_inp import TEST_FILE
 
 desired_width = 500
@@ -15,17 +12,17 @@ pd.set_option("display.max_columns", 30)
 
 def main():
     model = sw.Model(TEST_FILE, include_rpt=True)
-
+    print("SWMMIO Model Done!")
     with ps.Simulation(model.inp.path) as sim:
         for _ in sim:
             pass
+    print("Pyswm simulation done~!")
+    # conduits_data, nodes_data, subcatchments_data = feature_engineering(model)
 
-    conduits_data, nodes_data, subcatchments_data = feature_engineering(model)
-
-    swmm_model = SwmmModel(model, conduits_data, nodes_data, subcatchments_data)
+    # swmm_model = SwmmModel(model, conduits_data, nodes_data, subcatchments_data)
     # print(swmm_model.overflowing_traces())
     # print(swmm_model.all_traces())
-    print(swmm_model.overflowing_traces())
+    # print(swmm_model.overflowing_traces())
     # pprint.pprint(o.find_all_traces())
 
     # print(model.inp)
