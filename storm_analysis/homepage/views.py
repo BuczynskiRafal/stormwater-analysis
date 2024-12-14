ac92faa1b77d6f10f5bd94f0ebfa6afd41bddf93
BuczynskiRafal <buczynski.rafal2@gmail.com>
2024-12-14 06:22:41 +0100

    remove unnecessary files

--- storm_analysis/homepage/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-def index(request):
-    return render(request, "homepage/index.html")
-
-
-def about(request):
-    return render(request, "homepage/about.html")
